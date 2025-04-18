af3ac29f03d983db919826b23ee5662f030d9470
Adam Fidel <adam@fidel.id>
2025-04-18 16:42:03 -0500

    wrong clone

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdocs build -d ../static/95/docs
 cd ..
 
 if [ ! -d main/themes/re-terminal ]; then
-  git clone --depth 1 https://github.com/mirus-ua/hugo-theme-re-terminal.git winblues/themes/re-terminal
+  git clone --depth 1 https://github.com/mirus-ua/hugo-theme-re-terminal.git main/themes/re-terminal
 fi
 
 echo "Building Hugo..."
