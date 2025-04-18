ac882b6366adea7f21b17fc13e9d265a4e6684bc
Adam Fidel <adam@fidel.id>
2025-04-18 16:49:08 -0500

    fix base url

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+if [ -n "$DEPLOY_PRIME_URL" ]; then
+  # Netlify preview/deploy URL
+  BASE_URL="$DEPLOY_PRIME_URL"
+else
+  # Production URL
+  BASE_URL="https://blues.win"
+fi
+
 echo "Building MkDocs..."
 cd src
 mkdocs build -d ../static/95/docs
@@ -12,5 +20,5 @@ fi
 
 echo "Building Hugo..."
 cd main
-hugo -d ../static
+hugo --baseURL "$BASE_URL" -d ../static
 cd ..
